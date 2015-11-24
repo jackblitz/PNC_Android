5df805db1e1649a55107c75c62efbadcb9453f32
jackblitz <lukehopkinsdev@gmail.com>
2015-11-24 20:36:01 +0000

    main

--- app/src/main/java/com/hopkinsdev/howto/MainActivity.java
+++ app/src/main/java/com/hopkinsdev/howto/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends ActionBarActivity {
                     @Override
                     public void run() {
                         mAdView.loadAd(adRequest);
-                        mCatAdapter.addAdd();
+                        //mCatAdapter.addAdd();
                     }
                 });
             }
